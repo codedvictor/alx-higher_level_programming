e3d35e63ddfe39ce02743af3d320e9891ecaa922
Ige Victor <victorkoladeige@gmail.com>
2023-05-15 21:31:18 -0700

    last edit

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    
+    mul = []
+    for y in range(len(my_list)):
+        if my_list[y] % 2 == 0:
+            mul.append(True)
+        else:
+            mul.append(False)
+
+    return (mul)
