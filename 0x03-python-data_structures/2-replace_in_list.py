373e2277b546a320743a35f8e017048b2cd135be
Ige Victor <victorkoladeige@gmail.com>
2023-05-15 19:52:33 -0700

    new

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,4 +3,6 @@
 def replace_in_list(my_list, idx, element):
     if idx >= 0 & idx < (len(my_list)):
         my_list[idx] = element
+    else:
+        my_list[idx] = my_list[idx]
     return (my_list)
