ad1fa759843f3de7a383f13ae05832d61c678780
Ige Victor <victorkoladeige@gmail.com>
2023-05-18 20:40:05 -0700

    mandatory task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 def square_matrix_simple(matrix=[]):
     if matrix is None:
         return None
-    return list(list(map(lambda x: x** 2, y)) for y in matrix)
+    return list(list(map(lambda x: x ** 2, y)) for y in matrix)
