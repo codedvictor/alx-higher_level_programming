b4af828d5d4111402e6e78d4e092374c926444bf
Ige Victor <victorkoladeige@gmail.com>
2023-05-30 10:47:58 -0700

    eiditing

--- 0x06-python-classes/4_sq.py
+++ 0x06-python-classes/4_sq.py
@@ -10,11 +10,6 @@ class Square:
         """initialize object variable"""
         self.__size = size
 
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if (size < 0):
-            raise ValueError("size must be >= 0")
-
     def area(self):
         """calculate area of square"""
         size = self.__size
