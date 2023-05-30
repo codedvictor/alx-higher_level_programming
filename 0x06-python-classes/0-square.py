e173352ca93d0c3a6a8049f200ebf49eb66d4dbd
Ige Victor <victorkoladeige@gmail.com>
2023-05-30 07:06:09 -0700

    doc style updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""The sqaure class created"""
+    """The square class is created
+    just to permit block of python code to run through"""
 
 class Square:
     """pass statement added"""
     pass
+    #pass statement
