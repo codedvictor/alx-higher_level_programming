775b10f8dfa03ed06201486fa09f99afd2ff65fa
Ige Victor <victorkoladeige@gmail.com>
2023-05-30 08:21:53 -0700

    variable tyep exemption and if

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""The square class is created just to permit"""
+
+
+class Square:
+    """Setting private size data"""
+    pass
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if (size < 0 ):
+            raise ValueError("size must be >= 0")
