b9836d12c61aa3fd82e8b6caa0015980f983ed30
Ige Victor <victorkoladeige@gmail.com>
2023-05-30 07:47:28 -0700

    Square list hiding size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The square class is created just to permit"""
+
+
+class Square:
+    """Setting private size data"""
+    pass
+
+    def __init__(self, size):
+        self.__size = size
