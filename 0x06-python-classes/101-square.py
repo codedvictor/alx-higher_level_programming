1e2a9f127ff4500965066013bf41ff9e2ceed2ab
Ige Victor <victorkoladeige@gmail.com>
2023-05-31 14:53:26 -0700

    task 6 & 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Defines the square object class"""
+"""Defines the square class"""
 
 
 class Square:
     """Square class. Has a size"""
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize Square"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -23,12 +22,10 @@ class Square:
 
     @property
     def size(self):
-        """Return size of the square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set size of the square"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -57,6 +54,6 @@ class Square:
             for i in range(self.__position[1]):
                 print()
             st = '#' * self.__size
-            pt = ' ' * self.__position[0]
+            pr = ' ' * self.__position[0]
             for i in range(self.__size):
-                print(pt, st, sep="")
+                print(pr, st, sep="")
