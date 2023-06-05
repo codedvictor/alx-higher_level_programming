64d5663ad2b67d169d9f9a77cf6650f63dadbd97
Ige Victor <victorkoladeige@gmail.com>
2023-06-05 04:43:40 -0700

    Task 01

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+    """This is a rectangle class file"""
+    pass
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self._height = height
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("Height must be an integer")
+        if height < 0:
+            raise ValueError("Height must be >= 0")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >=0")
+        self.__width = value
