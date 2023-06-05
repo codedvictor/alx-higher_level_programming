2aefc5728893c8bea637eb7613f4cda0b33d95b4
Ige Victor <victorkoladeige@gmail.com>
2023-06-05 08:35:26 -0700

    string representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -60,7 +60,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         for x in range(self.__height):
-            stprt += str(Rectangle.print_symbol) * self.__width + "\n"
+            stprt += str(self.print_symbol) * self.__width + "\n"
         return (stprt[:-1])
 
     def __repr__(self):
