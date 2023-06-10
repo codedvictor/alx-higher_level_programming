6de6b332f26b87b580c55fa1059d729b8182badd
Ige Victor <victorkoladeige@gmail.com>
2023-06-09 21:20:49 -0700

    task 100 redo

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,5 @@
- #!/usr/bin/python3
-"""Defines a function  to multiply 2 matrices"""
+#!/usr/bin/python3
+"""Defines a function  to multiply matrix"""
 
 
 def matrix_mul(m_a, m_b):
@@ -9,12 +9,12 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    for x in m_a:
-        if not isinstance(x, list):
+    for row in m_a:
+        if not isinstance(row, list):
             raise TypeError("m_a must be a list of lists")
 
-    for x in m_b:
-        if not isinstance(x, list):
+    for row in m_b:
+        if not isinstance(row, list):
             raise TypeError("m_b must be a list of lists")
 
     if m_a == [] or m_a == [[]]:
@@ -23,26 +23,26 @@ def matrix_mul(m_a, m_b):
     if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
 
-    for x in m_a:
-        for i in x:
+    for row in m_a:
+        for i in row:
             if type(i) not in [int, float]:
                 raise TypeError("m_a should contain only integers or floats")
-    for x in m_b:
-        for i in x:
+    for row in m_b:
+        for i in row:
             if type(i) not in [int, float]:
                 raise TypeError("m_b should contain only integers or floats")
-    for x in m_a:
-        if len(x) != len(m_a[0]):
+    for row in m_a:
+        if len(row) != len(m_a[0]):
             raise TypeError("each row of m_a must be of the same size")
 
-    for x in m_b:
-        if len(x) != len(m_b[0]):
+    for row in m_b:
+        if len(row) != len(m_b[0]):
             raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise TypeError("m_a and m_b can't be multiplied")
 
-    mulvalue = [[sum(i * j for i, j in zip(x, y))
-             for y in zip(*m_b)] for x in m_a]
+    multmat = [[sum(i * j for i, j in zip(row, col))
+             for col in zip(*m_b)] for row in m_a]
 
-    return (mulvalue)
+    return (multmat)
