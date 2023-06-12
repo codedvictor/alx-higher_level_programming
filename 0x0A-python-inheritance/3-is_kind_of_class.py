dcda1d87d90019dfb0e29f918c7e7adf03fe362b
Ige Victor <victorkoladeige@gmail.com>
2023-06-12 14:08:08 -0700

    pycode error correction

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
     """function to check instance"""
-    if isinstance (obj, a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
