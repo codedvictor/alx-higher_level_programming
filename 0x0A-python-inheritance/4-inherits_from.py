1a61c503d07795ee0c2320d8083cce37f90869b4
Ige Victor <victorkoladeige@gmail.com>
2023-06-12 14:22:24 -0700

    only sub edit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """function to check instance"""
     if type(obj) is a_class:
         return False
-    return isinstance (obj, a_class)
+    return isinstance(obj, a_class)
