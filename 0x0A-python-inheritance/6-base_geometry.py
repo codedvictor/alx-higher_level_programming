08eec970d99feb85c683932e50bc328240f0893d
Ige Victor <victorkoladeige@gmail.com>
2023-06-12 14:41:18 -0700

    exception execution

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Geometry Module"""
+
+
+class BaseGeometry:
+    """No instance"""
+    pass
+
+    def area(self):
+        """No object"""
+        raise Exception("area() is not implemented")
