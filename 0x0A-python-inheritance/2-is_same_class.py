e3adaf533f3fe6df0808e8dbe823837f0316da3b
Ige Victor <victorkoladeige@gmail.com>
2023-06-12 14:01:30 -0700

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a function that returns True"""
+
+
+def is_same_class(obj, a_class):
+    """specified function"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
