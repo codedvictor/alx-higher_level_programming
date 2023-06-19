ae671f2a7c1601ce3366cc7bf825d73138285974
Ige Victor <victorkoladeige@gmail.com>
2023-06-19 15:23:15 -0700

    get attributes from dictionary

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,11 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+    
+    def to_dictionary(self):
+        dik = {}
+        get_attr = ['id', 'width', 'height', 'x', 'y']
+        for key in get_attr:
+            dik[key] = getattr(self, key)
+
+        return dik
