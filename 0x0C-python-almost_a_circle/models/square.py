45958b588bfaeb7a0e61a31e37d30aeb15d5ad2c
Ige Victor <victorkoladeige@gmail.com>
2023-06-19 15:43:07 -0700

    square inherit parent

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square inherit from rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class functions"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """set attibutes to parent attributes"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """square string representation"""
+        sq = "[Sqaure] ({}) {}/{} - ".format(self.id, self.x, self.y)
+        sz = "{}".format(self.width)
+        return (sq + sz)
