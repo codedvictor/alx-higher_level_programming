d92430eba4006b5b5a5486919b8a1bdda8879307
Ige Victor <victorkoladeige@gmail.com>
2023-08-14 21:56:21 -0700

    task 12 13

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myObj = {
+  type: 'object',
+  value: 12
+};
+console.log(myObj);
+
+myObj.value = 89;
+
+console.log(myObj);
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const add = function (a, b) {
+  return (a + b);
+};
+
+exports.add = add;
