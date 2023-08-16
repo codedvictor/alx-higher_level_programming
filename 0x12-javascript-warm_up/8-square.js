502eb683071b93d8ac83b4dea6d2155aeacd6c3d
Ige Victor <victorkoladeige@gmail.com>
2023-08-16 05:38:57 -0700

    fix missing space

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,12 +3,12 @@
 const value = parseInt(process.argv[2]);
 let size;
 
-if (Number.isNaN(value)){
+if (Number.isNaN(value)) {
   console.log('Missing size');
 } else {
-  for (let x = 0; x < value; x++){
+  for (let x = 0; x < value; x++) {
     size = '';
-    for (let y = 0; y < value; y++){
+    for (let y = 0; y < value; y++) {
       size += 'X';
     }
     console.log(size);
