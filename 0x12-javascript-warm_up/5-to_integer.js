68405604ec92532de239a3777d3dcaa194b5777f
Ige Victor <victorkoladeige@gmail.com>
2023-08-16 04:50:23 -0700

    fix missing space

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const value = parseInt(process.argv[2]);
 
-if (Number.isNaN(value)){
+if (Number.isNaN(value)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + process.argv[2]);
