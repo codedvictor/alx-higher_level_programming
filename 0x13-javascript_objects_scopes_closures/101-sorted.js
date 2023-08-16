749550446f76f1105532d2981a246ba31b6244e9
Ige Victor <victorkoladeige@gmail.com>
2023-08-16 04:27:24 -0700

    sort

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,7 @@
 const eDict = require('./101-data').dict;
 const nDict = {};
 
-Object.keys(eDict).map (function (key) {
+Object.keys(eDict).map ( function (key) {
   if (nDict[eDict[key]] === undefined) {
     nDict[eDict[key]] = [];
   }
