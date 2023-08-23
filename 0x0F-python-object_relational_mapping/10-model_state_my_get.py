bbd627aa207501c1f28f160606b746dff7786ec1
Ige Victor <victorkoladeige@gmail.com>
2023-08-23 10:50:44 -0700

    command line update

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,13 +8,13 @@ passed as argument from the database hbtn_0e_6_usa
 from sqlalchemy import (create_engine)
 from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
     new_engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                               format(sys.argv[1], sys.argv[2], sys.argv[3],
-                               sys.argv[4]), pool_pre_ping=True)
+                               format(sys.argv[1], argv[2], argv[3], argv[4]),
+                               pool_pre_ping=True)
 
     Session = sessionmaker(bind=new_engine)
     session = Session()
