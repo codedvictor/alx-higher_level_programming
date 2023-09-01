5b22d3103affa02cf8b3fac6b8394de64c5e2236
Ige Victor <victorkoladeige@gmail.com>
2023-09-01 06:59:29 -0700

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    req = requests.get(url)
+    html = reg.status_code
+    if html >= 400:
+        print('Error code: {}'.format(html))
+    else:
+        print(reg.text)
