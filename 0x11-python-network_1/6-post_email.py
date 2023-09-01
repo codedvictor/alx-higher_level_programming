432e4693deb6ba694236ec184283650047e236c1
Ige Victor <victorkoladeige@gmail.com>
2023-09-01 06:37:12 -0700

    post an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL, sends a request to the URL and
-displays the value of the variable X-Request-Id in
-the response header"""
+""" a Python script that takes in a URL and an email address, sends a POST
+request to the passed URL with the email as a parameter, and finally displays
+the body of the response."""
 import requests
 from sys import argv
 
@@ -13,4 +13,4 @@ if __name__ == "__main__":
 
     html = requests.post(url, data=val)
 
-    print("Your email is: {}".format(html.text))
+    print("Your email is: {}".format(html.data.text))
