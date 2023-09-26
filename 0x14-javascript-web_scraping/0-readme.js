44c7ec28ec51faae1ee5c4d0fb375c5bfa4f8912
Ige Victor Kolade <victorkoladeige@gmail.com>
2023-09-26 14:11:31 -0700

    semistandard error fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,14 +7,13 @@ if (process.argv.length !== 3) {
   process.exit(1);
 }
 
-const file_Path = process.argv[2];
+const filePath = process.argv[2];
 
 // Read and print the content of the file in utf-8 encoding
-fs.readFile(file_Path, 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
     console.log(data);
   }
 });
-
